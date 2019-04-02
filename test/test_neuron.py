b1b2f07604b982d9b4503692eda0aae6cd6b14de
bas <basilieus@gmail.com>
2019-04-02 12:01:45 -0700

    IFNeuron Test: Check for refractor values

--- test/test_neuron.py
+++ test/test_neuron.py
@@ -7,8 +7,11 @@ class TestIFNeuron:
         n = IFNeuron(shape=(2, 2), v_rest=v_rest)
         v_before_reset = n.v
         spikes_before_reset = n.spikes
+        refractor_count_before_reset = n.refractor_count
         n.reset()
         assert all(v_before_reset.flatten() == v_rest)
         assert all(n.v.flatten() == v_rest)
         assert all(spikes_before_reset.flatten() == 0)
         assert all(n.spikes.flatten() == 0)
+        assert all(refractor_count_before_reset.flatten() == 0)
+        assert all(n.refractor_count.flatten() == 0)
