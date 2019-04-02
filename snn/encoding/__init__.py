9d321925e602fd0205933a78b567ff237bd48fc5
bas <basilieus@gmail.com>
2019-04-02 12:12:08 -0700

    Use numpy vectorize for encoding

--- snn/encoding/__init__.py
+++ snn/encoding/__init__.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def kulkarni_rajendran(spikes: np.array) -> np.array:
+def kulkarni_rajendran(inpts: np.array) -> np.array:
     """
         Based on Kulkarnia and Rajendran encoding from:
         ElsevierNN_published_paper.pdf
@@ -10,4 +10,4 @@ def kulkarni_rajendran(spikes: np.array) -> np.array:
     def _encode(x):
         return 2700 + (x * 101.2)
 
-    return np.vectorize(_encode)(spikes)
+    return np.vectorize(_encode)(inpts)
