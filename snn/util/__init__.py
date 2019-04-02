a5cc7cf4ccd976d8b9f2fba6317bd1b1da006bae
bas <basilieus@gmail.com>
2019-04-02 12:59:33 -0700

    Move mnist into dataset

--- snn/util/__init__.py
+++ snn/util/__init__.py
@@ -4,12 +4,5 @@ from snn.util.convert import (
     pa_to_amperes,
     pa_to_nanoamps,
 )
-from snn.util.mnist import MNIST
 
-__all__ = [
-    "to_kernel",
-    "mv_to_volts",
-    "pa_to_amperes",
-    "pa_to_nanoamps",
-    "MNIST",
-]
+__all__ = ["to_kernel", "mv_to_volts", "pa_to_amperes", "pa_to_nanoamps"]
